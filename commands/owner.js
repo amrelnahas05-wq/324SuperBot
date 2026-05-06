92c76b2d64663f656c1381c477e51dbbf9c4e431
amrelnahas05-wq <amr.elnahas05@eng-st.cu.edu.eg>
2026-05-06 23:50:53 +0300

    fix: set owner contact to +201060715493, remove trailing message, fix waid format

--- commands/owner.js
+++ commands/owner.js
@@ -1,13 +1,9 @@
 const settings = require('../settings');
 
 async function ownerCommand(sock, chatId) {
-    const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-FN:${settings.botOwner}
-TEL;waid=${settings.ownerNumber}:${settings.ownerNumber}
-END:VCARD
-`;
+    const rawNumber = settings.ownerNumber.replace(/[^0-9]/g, '');
+
+    const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${settings.botOwner}\nTEL;waid=${rawNumber}:+${rawNumber}\nEND:VCARD`;
 
     await sock.sendMessage(chatId, {
         contacts: { displayName: settings.botOwner, contacts: [{ vcard }] },
